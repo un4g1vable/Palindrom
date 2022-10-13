3afd05a8d9612c1fffb24f04086d6d8754f18e86
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-13 15:50:20 +0500

    Add files via upload

---
+++ 13.10.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int Reverse(int n)
+{
+    int reverse = 0;
+    while (n) {
+        reverse *= 10;
+        reverse += n % 10;
+        n /= 10;
+    }
+    return reverse;
+}
+
+int main()
+{
+    setlocale(0, "");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+    cout << "Перевёрнутое число: " << Reverse(n) << endl;
+    if (n == Reverse(n))
+        cout << "Число палиднром" << endl;
+    else 
+        cout << "Число не палиднром" << endl;
+
+    return 0;
+}
